93087bd89ba01b1abca9936aae8128c94a34c287
Diego Luiz <diegoluizjava.java@gmail.com>
2016-04-27 14:42:04 -0300

    http plugin

--- expressProject/lib/app/controllers/index.js
+++ expressProject/lib/app/controllers/index.js
@@ -3,30 +3,30 @@ var services = require('../services');
 
 exports.insertUserPost = function(req, res){
 
-var categoria = req.body.categoria;
-var comentarios = req.body.comentarios;
-var severidade = req.body.severidade;
-var latitude = req.body.latitude;
-var longitude = req.body.longitude;
-var facebookId = req.body.facebookId;
+  var categoria = req.body.categoria;
+  var comentarios = req.body.comentarios;
+  var severidade = req.body.severidade;
+  var latitude = req.body.latitude;
+  var longitude = req.body.longitude;
+  var facebookId = req.body.facebookId;
 
-var alerta = new models.Alerta({
-  categoria: categoria,
-  comentarios: comentarios,
-  severidade: severidade,
-  latitude: latitude,
-  longitude: longitude,
-  facebookId: facebookId
-});
+  var alerta = new models.Alerta({
+    categoria: categoria,
+    comentarios: comentarios,
+    severidade: severidade,
+    latitude: latitude,
+    longitude: longitude,
+    facebookId: facebookId
+  });
 
-alerta.save(function(err, alerta){
+  alerta.save(function(err, alerta){
 
-  if(err){
-    throw err;
-  }
+    if(err){
+      throw err;
+    }
 
-  res.send(alerta);
-  
-});
+    res.send(alerta);
+
+  });
 
 }
